7aad47184b18684cb33273d79d6a25594e3e9e62
sharilyn <sharilyn.neidhardt@gmail.com>
2013-11-04 19:11:03 -0500

    inclass refactoring

--- 04_arrays/bob.rb
+++ 04_arrays/bob.rb
@@ -1,6 +1,28 @@
 class Bob
   def hey(message)
+    @message = message
+    
+    if screaming?
+        'Woah, chill out!'
+    elsif giving_silent_treatment?
+        'Fine. Be that way!'
+    elsif questioning?
+        'Sure.'
+    else
     "Whatever."
+    end
+  end
+
+  def screaming?
+    @message = @message.upcase && @message =~/[a-zA-Z]/
+  end
+
+  def giving_silent_treatment?
+    @message.strip.empty? 
+  end
+
+  def questioning?
+  @message.end_with? "?"
   end
 
 end
