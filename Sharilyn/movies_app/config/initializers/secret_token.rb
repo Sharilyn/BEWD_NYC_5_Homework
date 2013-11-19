dd496700eb1434503ef04190c0e309769de93b0e
sharilyn <sharilyn.neidhardt@gmail.com>
2013-11-18 21:35:25 -0500

    adding partially completed movies app

---
+++ Sharilyn/movies_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoviesApp::Application.config.secret_key_base = '7093571cd9154c5bc7ceebb4e70edcf9b30a290747b3fa4f608ca5d6b020580c174660db50c6bd4389d79f27eeed445657d51175c84de5b206b53f739bfdbfa7'
