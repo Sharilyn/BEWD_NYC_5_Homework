64fbca3e83375ee10f36f57f04abd8678e218b11
sharilyn <sharilyn.neidhardt@gmail.com>
2013-11-29 17:50:31 -0500

    tiny change to search

--- Sharilyn/movies_app/app/controllers/search_controller.rb
+++ Sharilyn/movies_app/app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     query = params[:q]
-    @movies = Movie.where('title LIKE :query OR description LIKE :query', query: "%#{query}%")
+    @movies = Movie.where('title LIKE :query OR description LIKE :query OR year_released LIKE :query', query: "%#{query}%")
   end
 end
